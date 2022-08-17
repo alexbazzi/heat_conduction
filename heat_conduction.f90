1a13900e27b0b912f4362672afef892b4a202c23
alexbazzi <alexanderbazzi@gmail.com>
2022-08-17 16:17:12 -0600

    Update heat_conduction.f90

--- heat_conduction.f90
+++ heat_conduction.f90
@@ -10,45 +10,41 @@ module heat_transfer
   integer :: i
   
   contains
-!========================SUBROUTINE fourier_oned========================
+!========================SUBROUTINE fourier_oned====================
 ! calculates temperature field on 1-D rod due to conduction
-!
-! Function to solve: q = k*dT/dx
-!
 !===================================================================
   subroutine fourier_oned(T, q, k, nx, dx, T_west)
     real(kind=p), dimension(nx), intent(inout) :: T
     real(kind=p), intent(in) :: q, T_west, k, dx
     integer, intent(in) :: nx
-    real(kind=p), dimension(:), allocatable :: T2 !temporary array to hold previos iteration values
+    real(kind=p), dimension(:), allocatable :: T_last !temporary array to hold previos iteration values
     real(kind=p) :: res = 0.5 !tolerance
-    integer :: count
+    integer :: count = 0
 
-    allocate(T2(nx))
-    T2(:) = T_west
+    allocate(T_last(nx))
+    T_last(:) = T_west
 
     residual_loop: do
       steady_state_loop: do i = 1, size(T)
         if (i .eq. size(T)) then !last node
-          T2(i) = 0.5*(q*k/dx + T(i-1))
+          T_last(i) = 0.5*(q*k/dx + T(i-1))
         else if (i .eq. 1) then !first node
-          T2(i) = 0.5*(T(i+1) + T_west)
+          T_last(i) = 0.5*(T(i+1) + T_west)
         else !interior nodes
-          T2(i) = 0.5*(T(i+1) + T(i-1))
+          T_last(i) = 0.5*(T(i+1) + T(i-1))
         end if
       end do steady_state_loop
       count = count + 1
-      print *, "residual = ", maxval(abs(T2-T))
-      
-      if (maxval(abs(T2 - T)) .le. res) then
-        T = T2
+      print *, "iteration = ", count, ",  residual = ", maxval(abs(T_last-T))
+
+      if (maxval(abs(T_last - T)) .le. res) then
+        T = T_last
         exit
       end if
 
-      T = T2
+      T = T_last
     end do residual_loop
   end subroutine fourier_oned
-
 end module heat_transfer
 
 program heat_conduction
@@ -57,9 +53,7 @@ program heat_conduction
   integer :: nx
   real(kind=p) :: q, k, T_west, dx
   real(kind=p), dimension(:), allocatable :: T
-
-  !print *, "Enter number of nodes (nx): "
-  !read(*,*) nx
+  
   nx = 5
   allocate(T(nx))
   T_west = 300.0 !room temp in Kelvin
@@ -74,6 +68,5 @@ program heat_conduction
   open(1, file="temperature_profile.dat")
   write(1,*) T
   close(1)
-
 
 end program heat_conduction
